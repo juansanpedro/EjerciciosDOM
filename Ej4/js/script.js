9623504a379296e4d4b10635af9d91d778cf1567
juansanpedro <juanignacio50@hotmail.com>
2022-11-27 22:53:47 -0300

    .

--- Ej4/js/script.js
+++ Ej4/js/script.js
@@ -7,5 +7,11 @@ const DATA = [
 ];
 
 // Escribe el código necesario aquí
-
+const container = document.getElementById("container");
+function agregarParrafo(per) {    //Función que agrega el <p> al <div> de "id=container"
+  container.innerHTML += '<p>'+ per.name + ' ' + per.lastname + '</p>';
+}
+for (const personas of DATA) {    //Recorre el array "DATA" y llama a la función agregarParrafo
+  agregarParrafo(personas); 
+}
 //
